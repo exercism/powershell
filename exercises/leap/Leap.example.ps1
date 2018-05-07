c407efc4087531716bd3c222b45823a65b17cf96
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-08 00:20:19 +0200

    Update Leap.example.ps1

--- exercises/leap/Leap.example.ps1
+++ exercises/leap/Leap.example.ps1
@@ -6,9 +6,9 @@ function Test-LeapYear {
 
 	# Instead solve using math
 	if ( $year % 4 -eq 0 -and $year % 100 -ne 0 -or $year % 400 -eq 0) {
-		return "True"
+		return True
 	}
 	else {
-		return "False"
+		return False
 	}
 }
