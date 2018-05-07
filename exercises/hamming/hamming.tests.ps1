5f092a122a5ac966d942c12baf988507fa0cc22b
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-08 00:48:19 +0200

    Update hamming.tests.ps1

--- exercises/hamming/hamming.tests.ps1
+++ exercises/hamming/hamming.tests.ps1
@@ -4,61 +4,61 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.tests\.', '.'
 
 Describe "HammingTest" {
 	It "tests identical strands" {
-		Compute 'A' 'A' | Should be 0
+		Get-HammingDifference 'A' 'A' | Should be 0
 	}
 	It "tests log identical strands" {
-		Compute "GGACTGA" "GGACTGA" | Should be 0
+		Get-HammingDifference "GGACTGA" "GGACTGA" | Should be 0
 	}
 
 	It "tests complete distance in single nucleotide strands" {
-		Compute "A" "G" | Should be 1
+		Get-HammingDifference "A" "G" | Should be 1
 	}
 
 	It "tests complete distance in small strands" {
-		Compute "AG" "CT" | Should be 2
+		Get-HammingDifference "AG" "CT" | Should be 2
 	}
 
 	It "tests small distance in small strands" {
-		Compute "AT"  "CT" | Should be 1
+		Get-HammingDifference "AT"  "CT" | Should be 1
 	}
 
 	It "tests small distance" {
-		Compute "GGACG" "GGTCG" | Should be 1
+		Get-HammingDifference "GGACG" "GGTCG" | Should be 1
 	}
 
 	It "tests small distance in long strands" {
-		Compute "ACCAGGG" "ACTATGG" | Should be 2
+		Get-HammingDifference "ACCAGGG" "ACTATGG" | Should be 2
 	}
 
 	It "tests non unique character in first strand" {
-		Compute "AGA" "AGG" | Should be 1
+		Get-HammingDifference "AGA" "AGG" | Should be 1
 	}
 
 	It "tests non unique character in second strand" {
-		Compute "AGG" "AGA" | Should be 1
+		Get-HammingDifference "AGG" "AGA" | Should be 1
 	}
 
 	It "tests same nucleotides in different position" {
-		Compute "TAG" "GAT" | Should be 2
+		Get-HammingDifference "TAG" "GAT" | Should be 2
 	}
 
 	It "tests large distance" {
-		Compute "GATACA" "GCATAA" | Should be 4
+		Get-HammingDifference "GATACA" "GCATAA" | Should be 4
 	}
 
 	It "tests large distance in off by one strand" {
-		Compute "GGACGGATTCTG" "AGGACGGATTCT" | Should be 9
+		Get-HammingDifference "GGACGGATTCTG" "AGGACGGATTCT" | Should be 9
 	}
 
 	It "tests empty strands" {
-		Compute "" "" | Should be 0
+		Get-HammingDifference "" "" | Should be 0
 	}
 
 	It "tests disallow first strand longer" {
-		{ Compute "AATG" "AAA" } | Should Throw "Mismatching string lengths"
+		{ Get-HammingDifference "AATG" "AAA" } | Should Throw "Mismatching string lengths"
 	}
 
 	It "tests disallow second strand longer" {
-		{ Compute "ATA" "AGTG" } | Should Throw "Mismatching string lengths"
+		{ Get-HammingDifference "ATA" "AGTG" } | Should Throw "Mismatching string lengths"
 	}
 }
