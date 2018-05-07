dfb7e37e49b281ada7f8f63fa3dec2d0ae902d21
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-08 00:05:04 +0200

    Name convetion enforcement

--- exercises/two-fer/TwoFer.tests.ps1
+++ exercises/two-fer/TwoFer.tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.tests\.', '.'
 . "$here\$sut"
 
-Describe "Get-TwoFerTests" {
+Describe "GetTwoFerTests" {
 	It "Enter1stTestHere" {
 		Throw "Tests not implemented"
 	}
