f94172d5d4f46374f55ee9cd0a32998ef6feaeb8
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-08 19:03:06 +0200

    Tab vs Space normalization

--- exercises/two-fer/TwoFer.tests.ps1
+++ exercises/two-fer/TwoFer.tests.ps1
@@ -5,32 +5,32 @@ $CommandName = "Get-TwoFer"
 
 # Remove the function if its already found
 If (Get-Command $CommandName -ErrorAction SilentlyContinue){
-	Write-Verbose "Removing the existing $CommandName function as it exists"
-	Remove-Item -Path "Function:\$CommandName"
+    Write-Verbose "Removing the existing $CommandName function as it exists"
+    Remove-Item -Path "Function:\$CommandName"
 }
 
 # Load the script file
 If (Test-Path "$ExercisePath\$ScriptFile"){
-	Write-Output ("Loading: {0}" -f "$ExercisePath\$ScriptFile")
-	. ("$ExercisePath\$ScriptFile")
+    Write-Output ("Loading: {0}" -f "$ExercisePath\$ScriptFile")
+    . ("$ExercisePath\$ScriptFile")
 }
 Else {
-	# Display an error and stop the tests
-	Write-Error "The file $ScriptFile was not found. You need to create your answer in a file named $ScriptFile" -ErrorAction Stop
+    # Display an error and stop the tests
+    Write-Error "The file $ScriptFile was not found. You need to create your answer in a file named $ScriptFile" -ErrorAction Stop
 }
 
 
 Describe "Get-TwoFer" {
-	
-	It "Given <Name> expects <Expected>" -TestCases @(
-		@{ Name = $null; Expected = "One for you, one for me" },
-		@{ Name = ""; Expected = "One for you, one for me" },
-		@{ Name = "Alice"; Expected = "One for Alice, one for me" }
-	) {
-		Param(
-			$Name, $Expected
-		)
 
-		Get-TwoFer -Name $Name | Should -Be $Expected
-	}
+    It "Given <Name> expects <Expected>" -TestCases @(
+        @{ Name = $null; Expected = "One for you, one for me" },
+        @{ Name = ""; Expected = "One for you, one for me" },
+        @{ Name = "Alice"; Expected = "One for Alice, one for me" }
+    ) {
+        Param(
+            $Name, $Expected
+        )
+
+        Get-TwoFer -Name $Name | Should -Be $Expected
+    }
 }
