9ecc17c22dbb4458ce7e4b5cd4f806a935c3be77
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-08 19:13:26 +0200

    HelloWord-Normalization (#53)
    HelloWord-Normalization (#53)

---
+++ exercises/hello-world/HelloWorld.example.ps1
@@ -0,0 +1,5 @@
+function Get-HelloWorld {
+	param( [string]$person = "World")
+
+	return "Hello, $person!"
+}
---
+++ exercises/hello-world/HelloWorld.ps1
@@ -0,0 +1,5 @@
+function Get-HelloWorld {
+	param([string]$person = "World")
+	
+    Throw "Exercise not implemented"
+}
