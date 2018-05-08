778319c0f4df284d3f3b487f7b04488bb2e24001
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-08 22:53:09 +0200

    Typo. Issue#57

--- exercises/nucleotide-count/NucleotideCount.tests.ps1
+++ exercises/nucleotide-count/NucleotideCount.tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.tests\.', '.'
 . "$here\$sut"
 
-Describe "Get-SumOfMultiplesTests" {
+Describe "NucleotideCountTests" {
 	It "Enter1stTestHere" {
         #Get-NucleotideCount
 		Throw "Not implemented exception"
