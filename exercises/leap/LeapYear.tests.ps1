3770cbe4a4f97698bd3488e86603d667f2ef5df7
Christian P. Gyssels <gyssels@dekeract.com>
2018-05-18 15:42:45 +0200

    Update LeapYear.tests.ps1 (#60)
    Aligned the tests to the problem specification on: https://github.com/exercism/problem-specifications/blob/master/exercises/leap/canonical-data.json

--- exercises/leap/LeapYear.tests.ps1
+++ exercises/leap/LeapYear.tests.ps1
@@ -3,31 +3,19 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.tests\.', '.'
 . "$here\$sut"
 
 Describe "LeapYear Tests" {
-    It "tests leap year" {
+    It "Year not divisible by 4: common year" {
+        Test-LeapYear(2015) | Should be $false
+    }
+
+    It "Year divisible by 4, not divisible by 100: leap year" {
         Test-LeapYear(1996) | Should be $true
     }
 
-    It "tests standard and odd year" {
-        Test-LeapYear(1997) | Should be $false
+    It "Year divisible by 100, not divisible by 400: common year" {
+        Test-LeapYear(2100) | Should be $false
     }
 
-    It "tests standard even year" {
-        Test-LeapYear(1998) | Should be $false
-    }
-
-    It "tests standard ninteenth century" {
-        Test-LeapYear(1900) | Should be $false
-    }
-
-    It "tests standard eighteenth year" {
-        Test-LeapYear(1800) | Should be $false
-    }
-
-    It "tests leap twenty fourth century" {
-        Test-LeapYear(2400) | Should be $true
-    }
-
-    It "tests leap year 2000" {
+    It "Year divisible by 400: leap year" {
         Test-LeapYear(2000) | Should be $true
     }
 }
