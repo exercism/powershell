cf73d98afffca2e0b53403147c8566e77d117d12
Kent Chenery <kent.chenery@enterpriseit.co.nz>
2018-06-07 13:59:04 +1200

    Implement the Sum of Multiples exercise.

--- exercises/sum-of-multiples/SumOfMultiples.example.ps1
+++ exercises/sum-of-multiples/SumOfMultiples.example.ps1
@@ -1,3 +1,38 @@
-function Get-SumOfMultiples() {
-  Throw "Function not implemented"
+<#
+.SYNOPSIS
+Given a number, find the sum of all the unique multiples of particular numbers up to
+but not including that number.
+
+.DESCRIPTION
+If we list all the natural numbers below 20 that are multiples of 3 or 5,
+we get 3, 5, 6, 9, 10, 12, 15, and 18.
+
+.PARAMETER Multiples
+An array of the factors 
+
+.PARAMETER Limit
+The value BELOW which we test for
+
+.EXAMPLE
+Get-SumOfMultiples -Multiples @(3, 5) -Limit 10
+#>
+Function Get-SumOfMultiples {
+    [CmdletBinding()]
+    Param(
+        [int[]]$Multiples,
+        [int]$Limit
+    )
+
+    $result = 0;
+
+    for($i = 1; $i -lt $Limit; $i ++) {
+        foreach($multiple in $Multiples) {
+            if ($i % $multiple -eq 0) {
+                $result += $i
+                break   # Break out of the foreach look because we've found a factor
+            }
+        }
+    }
+
+    Return $result;
 }
