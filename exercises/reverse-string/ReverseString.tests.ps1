a5eb21ae61adc9795595ad673d7e262b0abcefb4
kchenery <kent@chenery.net.nz>
2018-06-07 22:29:23 +1200

    Implement the ReverseString exercise

--- exercises/reverse-string/ReverseString.tests.ps1
+++ exercises/reverse-string/ReverseString.tests.ps1
@@ -2,8 +2,20 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.tests\.', '.'
 . "$here\$sut"
 
-Describe "ReverseStringTests" {
-	It "Enter1stTestHere" {
-		Throw "Tests not implemented"
+Describe "Get-ReverseString Tests" {
+	It "Given <Forward> it outputs <Reverse>" -TestCases @(
+		@{ Forward = ""; Reverse = "" },
+		@{ Forward = "PowerShell"; Reverse = "llehSrewoP" },
+		@{ Forward = "robot"; Reverse = "tobor" },
+		@{ Forward = "Ramen"; Reverse = "nemaR" },
+		@{ Forward = "I'm hungry!"; Reverse = "!yrgnuh m'I" },
+		@{ Forward = "racecar"; Reverse = "racecar" }
+	) {
+		Param(
+			[string]$Forward,
+			[string]$Reverse
+		)
+
+		Get-ReverseString -Forward $Forward | Should -BeExactly $Reverse
 	}
 }
