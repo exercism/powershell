506744c56aed9b74707cc021ddd3e3d12d175068
Christian P. Gyssels <gyssels@dekeract.com>
2018-06-11 22:51:34 +0200

    Update ReverseString.ps1
    The comments submitted should be in the read.me file (autogenerated by configlet).

--- exercises/reverse-string/ReverseString.ps1
+++ exercises/reverse-string/ReverseString.ps1
@@ -1,16 +1,3 @@
-<#
-.SYNOPSIS
-Reverse a string
-
-.DESCRIPTION
-Reverses the string in its entirety. That is it does not reverse each word in a string individually.
-
-.PARAMETER Forward
-The string to be reversed
-
-.EXAMPLE
-Get-ReverseString "PowerShell"
-#>
 Function Get-ReverseString {
     [CmdletBinding()]
     Param(
