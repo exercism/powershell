96f906e1488868e1b8a8b0cf3757365e0064e0ca
Christian P. Gyssels <gyssels@dekeract.com>
2018-06-12 01:00:07 +0200

    Update ReverseString.example.ps1

--- exercises/reverse-string/ReverseString.example.ps1
+++ exercises/reverse-string/ReverseString.example.ps1
@@ -1,3 +1,16 @@
+<#
+.SYNOPSIS
+Reverse a string
+
+.DESCRIPTION
+Reverses the string in its entirety. That is it does not reverse each word in a string individually.
+
+.PARAMETER Forward
+The string to be reversed
+
+.EXAMPLE
+Get-ReverseString "PowerShell"
+#>
 Function Get-ReverseString {
     [CmdletBinding()]
     Param(
