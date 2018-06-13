5d54a163f1d2cabb7ece64a650c56b2894a8c11a
Kent Chenery <kent.chenery@enterpriseit.co.nz>
2018-06-14 09:52:31 +1200

    Add comment based help to the functions.

--- exercises/hello-world/HelloWorld.ps1
+++ exercises/hello-world/HelloWorld.ps1
@@ -1,4 +1,17 @@
 function Get-HelloWorld {
+    <#
+    .SYNOPSIS
+    Outputs "Hello World"
+    
+    .DESCRIPTION
+    Output "Hello World" or "Hello <Name>" if Name is supplied to the CmdLet.
+    
+    .PARAMETER person
+    Name of the person to display. If not supplied, Hello World should be displayed
+    
+    .EXAMPLE
+    Get-HelloWorld
+    #>
 	param([string]$person = "World")
 	
     Throw "Exercise not implemented"
