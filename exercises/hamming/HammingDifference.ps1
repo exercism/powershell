3b0888727c04a640f33dea8212864b083781a6d4
Kent Chenery <kent@chenery.net.nz>
2018-06-15 22:51:51 +1200

    The exercise function contains a solution. This should be in the example version. (#87)
    Good!

--- exercises/hamming/HammingDifference.ps1
+++ exercises/hamming/HammingDifference.ps1
@@ -1,14 +1,3 @@
 function Get-HammingDifference([string]$strand1, [string]$strand2) {
-  if ( $strand1.length -ne $strand2.length) {
-    Throw "Mismatching string lengths"
-  }
-  $first = [char[]][string]$strand1
-  $second = [char[]][string]$strand2
-  $result = 0
-  for ($i=0; $i -lt $first.length; $i++) {
-    if ( $first[$i] -ne $second[$i] ) {
-      $result += 1
-    }
-  }
-  return $result
+    Throw "Exercise not implemented"
 }
