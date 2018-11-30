3323dc4721881a0546679fae2b4a5116a2b9e2aa
Marcus Crane <marcus@utf9k.net>
2018-11-30 22:43:03 +1300

    Correct test description type and updated function guide to match what the tests expect (#98)
    Thank you!

--- exercises/hello-world/HelloWorld.ps1
+++ exercises/hello-world/HelloWorld.ps1
@@ -1,10 +1,10 @@
 function Get-HelloWorld {
     <#
     .SYNOPSIS
-    Outputs "Hello World"
+    Outputs "Hello, World!"
     
     .DESCRIPTION
-    Output "Hello World" or "Hello <Name>" if Name is supplied to the CmdLet.
+    Output "Hello, World!" or "Hello, <Name>!" if Name is supplied to the CmdLet.
     
     .PARAMETER person
     Name of the person to display. If not supplied, Hello World should be displayed
