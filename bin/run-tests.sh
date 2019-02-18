8100c48dd6425d1d01007e52c859fcc7119691cb
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-18 11:35:12 -0500

    nope, not xenial; ditch unbuffer until workaround is found
    "On second thought, let's not use xenial. Tis' a silly distro."

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -7,7 +7,7 @@ for exercise_directory in $(find ./exercises/* -type d); do
     solution_file=$(find "$exercise_directory" -type f -name '*.example.ps1')
     cp $(printf "%s" "$test_file") "$tmpdir"
     cp $(printf "%s" "$solution_file") "$tmpdir/$(basename "${test_file//.tests}")"
-    results="$(unbuffer pwsh -WorkingDirectory "$tmpdir" -Command 'Invoke-Pester' | tee /dev/tty)"
+    results="$(pwsh -WorkingDirectory "$tmpdir" -Command 'Invoke-Pester' | tee /dev/tty)"
     rm -rf "$tmpdir"
     failures="$(grep -oP '(?<=Failed: )[[:digit:]]+' <<< "$results")"
     if [ "$failures" -ne '0' ]; then
