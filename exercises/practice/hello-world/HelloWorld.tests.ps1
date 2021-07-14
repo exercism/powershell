9675db91dfc84240153b297ec2cf0676b2487c8a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-14 14:34:55 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible. (#150)

--- exercises/practice/hello-world/HelloWorld.tests.ps1
+++ exercises/practice/hello-world/HelloWorld.tests.ps1
@@ -1,17 +1,9 @@
 BeforeAll {
-    . ".\HelloWorld.ps1"
+	. ".\HelloWorld.ps1"
 }
 
 Describe "HelloWorldTest" {
 	It "Outputs: 'Hello, World!'" {
 		Get-HelloWorld | Should -Be 'Hello, World!'
-	}
-
-	It "Outputs: Hello, Alice!" {
-		Get-HelloWorld 'Alice' | Should -Be 'Hello, Alice!'
-	}
-
-	It "Outputs: Hello, Bob!" {
-		Get-HelloWorld -person 'Bob' | Should -Be 'Hello, Bob!'
 	}
 }
