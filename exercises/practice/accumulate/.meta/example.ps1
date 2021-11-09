edb7f4a65baffd14f431c8f6cf2a644da6a6827c
Kryo <kryo@rockyos.Home>
2021-11-09 11:34:43 -0600

    accumulate: added new powershell exercise

---
+++ exercises/practice/accumulate/.meta/example.ps1
@@ -0,0 +1,41 @@
+Function Get-Accumulation() {
+    <#
+    .SYNOPSIS
+    Accumulate a list of numbers based on the operation given.
+    
+    .DESCRIPTION
+    Given the collection of numbers:
+
+    - 1, 2, 3, 4, 5
+
+    And the operation:
+
+    - square a number (`x => x * x`)
+
+    Your code should be able to produce the collection of squares:
+
+    - 1, 4, 9, 16, 25
+
+    Check out the test suite to see the expected function signature.
+    
+    .PARAMETER List
+    Collection of numbers to perform an operation on.
+    
+    .PARAMETER Func
+    A scriptblock containing the operation to perform on the given $List
+    
+    .EXAMPLE
+    Get-Accumulation -List 1,2,3,4,5 -Func { <some code here> }
+    #>
+    [CmdletBinding()]
+    Param(
+        [Int64[]]$List,
+        [scriptblock]$Func
+    )
+
+    $Func.Invoke($List)
+}
+
+Get-Accumulation -List 1,2,3,4,5 -Func { Param([Int64[]]$numbers) $numbers | ForEach-Object { $_ * $_ }}
+
+Get-Accumulation -List 1,2,3,4,5 -Func { Param([Int64[]]$numbers) $numbers | ForEach-Object { $_ + $_ }}
---
+++ exercises/practice/accumulate/Get-Accumulation.tests.ps1
@@ -0,0 +1,23 @@
+BeforeAll {
+    . "./Get-Accumulation.ps1"
+}
+
+Describe 'Test Get-Accumulation.ps1' {
+    It "square numbers" {
+        $got = @()
+        $got += Get-Accumulation -List 1,2,3,4,5 -Func { Param([Int64[]]$numbers) $numbers | ForEach-Object { $_ * $_ }} 
+
+        $want = @(1,4,9,16,25)
+
+        $got | Should -BeExactly $want
+    }
+
+    It "add numbers" {
+        $got = @()
+        $got += Get-Accumulation -List 1,2,3,4,5 -Func { Param([Int64[]]$numbers) $numbers | ForEach-Object { $_ + $_ }} 
+
+        $want = @(2,4,6,8,10)
+
+        $got | Should -BeExactly $want
+    }
+}
