872b172b794f9cdcc81b5cbdb4444c4a1fadc588
Kaleb Hawkins <KalebHawkins@outlook.com>
2021-11-17 07:34:05 -0600

    Add Collatz Conjecture Exercise (#170)
    - Update config.json to include exercise slug.
      - Create the required files and directory structure for exercise.
      - Fill out instructions.md.
      - Configure $ExerciseDir/.meta/config.json file.
      - Add exercise stub.
      - Add exercise tests.
      - Add example solution.
    
    Linting: Tested
        ./bin/configlet lint
        The lint command is under development.
        Please re-run this command regularly to see if your track passes the latest linting rules.
    
        Basic linting finished successfully:
        - config.json exists and is valid JSON
        - config.json has these valid fields:
            language, slug, active, blurb, version, status, online_editor, key_features, tags
        - Every concept has the required .md files
        - Every concept has a valid links.json file
        - Every concept has a valid .meta/config.json file
        - Every concept exercise has the required .md files
        - Every concept exercise has a valid .meta/config.json file
        - Every practice exercise has the required .md files
        - Every practice exercise has a valid .meta/config.json file
        - Required track docs are present
        - Required shared exercise docs are present
    
    Pester: Tested
        ./bin/run-tests.ps1
    
        ```
        Starting discovery in 1 files.
        Discovery found 6 tests in 5ms.
        Running tests.
        [+] /tmp/collatz-conjecture/collatzConjecture.tests.ps1 38ms (16ms|17ms)
        Tests completed in 39ms
        Tests Passed: 6, Failed: 0, Skipped: 0 NotRun: 0
        ```
    
    Co-authored-by: Kryo <kryo@rockyos.Home>

---
+++ exercises/practice/collatz-conjecture/CollatzConjecture.ps1
@@ -0,0 +1,21 @@
+Function Invoke-CollatzConjecture() {
+    <#
+    .SYNOPSIS
+    Calculate the number of steps to reach 1 using the Collatz conjecture.
+
+    .DESCRIPTION
+    Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+
+    .PARAMETER Number
+    The number to perform the Collatz Conjecture function on.
+
+    .EXAMPLE
+    Invoke-CollatzConjecture -Number 12
+    #>
+    [CmdletBinding()]
+    Param(
+        [Int64]$Number
+    )
+
+    Throw "Exercise not implemented"
+}
---
+++ exercises/practice/collatz-conjecture/CollatzConjecture.tests.ps1
@@ -0,0 +1,46 @@
+BeforeAll {
+    . "./CollatzConjecture.ps1"
+}
+
+Describe "Test Invoke-CollatzConjecture.ps1" {
+    Context "Passing Tests" {
+
+        It "zero steps for one" {
+            $got = Invoke-CollatzConjecture -Number 1
+            $want = 0
+    
+            $got | Should -BeExactly $want
+        }
+    
+        It "divide if even" {
+            $got = Invoke-CollatzConjecture -Number 16
+            $want = 4
+    
+            $got | Should -BeExactly $want
+        }
+    
+        It "even and odd steps" {
+            $got = Invoke-CollatzConjecture -Number 12
+            $want = 9
+    
+            $got | Should -BeExactly $want
+        }
+        
+        It "large number of even and odd steps" {
+            $got = Invoke-CollatzConjecture -Number 1000000
+            $want = 152
+    
+            $got | Should -BeExactly $want
+        }
+    }
+    
+    Context "Failing Tests" {
+        It "zero is an error" {
+            { Invoke-CollatzConjecture -Number 0 } | Should -Throw "error: Only positive numbers are allowed"
+        }
+    
+        It "negative value is an error" {
+            { Invoke-CollatzConjecture -Number 0 } | Should -Throw "error: Only positive numbers are allowed"
+        }
+    }
+}
