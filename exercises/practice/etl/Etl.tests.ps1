1a2a4bdb35df7cc77648c02171aa65b8d45494ee
Carl <meatballsofmeatballs@gmail.com>
2023-06-28 22:04:36 +0200

    Shorten the test file

--- exercises/practice/etl/Etl.tests.ps1
+++ exercises/practice/etl/Etl.tests.ps1
@@ -43,32 +43,10 @@ Describe "Test Invoke-Etl.ps1" {
                                     5 = @("K")
                                     8 = @("J", "X")
                                     10 = @("Q", "Z")}
-        $want = @{a = 1
-                  b = 3
-                  c = 3
-                  d = 2
-                  e = 1
-                  f = 4
-                  g = 2
-                  h = 4
-                  i = 1
-                  j = 8
-                  k = 5
-                  l = 1
-                  m = 3
-                  n = 1
-                  o = 1
-                  p = 3
-                  q = 10
-                  r = 1
-                  s = 1
-                  t = 1
-                  u = 1
-                  v = 4
-                  w = 4
-                  x = 8
-                  y = 4
-                  z = 10}
+        $want = @{a = 1; b = 3; c = 3; d = 2; e = 1; f = 4; g = 2; h = 4; i = 1
+                  j = 8; k = 5; l = 1; m = 3; n = 1; o = 1; p = 3; q = 10; r = 1
+                  s = 1; t = 1; u = 1; v = 4; w = 4; x = 8; y = 4; z = 10
+                  }
 
         $want.Keys | Should -HaveCount $got.Keys.Count
         $want.Keys | % {$want[$_] | Should -Be $got[$_]}
