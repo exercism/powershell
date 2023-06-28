efd62f34a8592a86951cc7d611e150fc6f1a372d
Carl <meatballsofmeatballs@gmail.com>
2023-06-28 21:22:11 +0200

    Update exercises batch 2

--- exercises/practice/nucleotide-count/NucleotideCount.ps1
+++ exercises/practice/nucleotide-count/NucleotideCount.ps1
@@ -8,9 +8,10 @@ Function Get-NucleotideCount() {
     DNA is a large molecule that is built from an extremely long sequence of individual elements called nucleotides.
     4 types exist in DNA and these differ only slightly and can be represented as the following symbols: 'A' for adenine, 'C' for cytosine, 'G' for guanine, and 'T' thymine.
 
-    The function counts the occurances of A, C, G and T in the supplied strand. It then outputs in the format:
+    The function counts the occurances of A, C, G and T in the supplied strand.
+    And returns a hashtable in the format:
 
-    A:0, C:0, G:0, T:0
+    @{ A = 2; C = 2; G = 2; T = 3 }
     
     .PARAMETER Strand
     The DNA strand to count
@@ -18,7 +19,7 @@ Function Get-NucleotideCount() {
     .EXAMPLE
     Get-NucleotideCount -Strand "ACGTAGCTT"
     
-    Retuns: A:2 C:2 G:2 T:3
+    Retuns: @{ A = 2; C = 2; G = 2; T = 3 }
     #>
     [CmdletBinding()]
     Param(
