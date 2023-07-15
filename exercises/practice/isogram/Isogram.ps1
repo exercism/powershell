acac912f9b7879d3b297f7fc1890922d15faac1c
meatball <meatballsofmeatballs@gmail.com>
2023-07-15 23:44:42 +0200

    Add isogram

---
+++ exercises/practice/isogram/Isogram.ps1
@@ -0,0 +1,38 @@
+Function Invoke-Isogram() {
+    <#
+    .SYNOPSIS
+    Determine if a word or phrase is an isogram.
+    
+    .DESCRIPTION
+    An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter,
+    however spaces and hyphens are allowed to appear multiple times.
+    
+    
+    .PARAMETER Phrase
+    The DNA strand to count
+    
+    .EXAMPLE
+    Invoke-Isogram -Phrase "isogram"
+    
+    Retuns: $true
+    #>
+    [CmdletBinding()]
+    Param(
+        [string]$Phrase
+    )
+
+    $Phrase = $Phrase.ToLower()
+    $Phrase = $Phrase.Replace(" ", "")
+    $Phrase = $Phrase.Replace("-", "")
+
+    $FoundLetters = @()
+    $Letters = $Phrase.ToCharArray() 
+    foreach ($item in $Letters) {
+        if ($FoundLetters -contains $item) {
+            return $false
+        }
+        $FoundLetters += $item
+    }
+    return $true
+}
+
