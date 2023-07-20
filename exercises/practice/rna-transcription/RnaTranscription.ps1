7d604c976dbd29cb0f248e2b2cbdf3f930b7c14b
meatball <69751659+meatball133@users.noreply.github.com>
2023-07-20 10:17:41 +0200

    Add RnaTranscription (#225)

---
+++ exercises/practice/rna-transcription/RnaTranscription.ps1
@@ -0,0 +1,21 @@
+Function Invoke-RnaTranscription() {
+    <#
+    .SYNOPSIS
+    Transcribe a DNA strand into RNA.
+
+    .DESCRIPTION
+    Given a DNA strand, return its RNA complement (per RNA transcription).
+
+    .PARAMETER Strand
+    The DNA strand to transcribe.
+
+    .EXAMPLE
+    Invoke-RnaTranscription -Strand "A"
+    #>
+    [CmdletBinding()]
+    Param(
+        [string]$Strand
+    )
+    
+    throw "Please implement this function"
+}
