6c61964df50edb543559a1c657558345b1c7a28d
glaxxie <86179463+glaxxie@users.noreply.github.com>
2023-07-23 14:02:55 -0500

    Add Acronym (#229)
    * Add Acronym
    
    * fix some errors

---
+++ exercises/practice/acronym/.meta/Acronym.example.ps1
@@ -0,0 +1,21 @@
+Function Get-Acronym() {
+    <#
+    .SYNOPSIS
+    Get the acronym of a phrase.
+
+    .DESCRIPTION
+    Given a phrase, return the string acronym of that phrase.
+    "As Soon As Possible" => "ASAP"
+    
+    .PARAMETER Phrase
+    The phrase to get the acronym from.
+    
+    .EXAMPLE
+    Get-Acronym -Phrase "As Soon As Possible"
+    #>
+    [CmdletBinding()]
+    Param (
+        [string]$Phrase
+    )
+    return ([regex]::Matches($Phrase, "\p{L}+'?\p{L}*") | ForEach-Object { $_.Value[0] }) -join ""
+}
---
+++ exercises/practice/acronym/Acronym.ps1
@@ -0,0 +1,21 @@
+Function Get-Acronym() {
+    <#
+    .SYNOPSIS
+    Get the acronym of a phrase.
+
+    .DESCRIPTION
+    Given a phrase, return the string acronym of that phrase.
+    "As Soon As Possible" => "ASAP"
+    
+    .PARAMETER Phrase
+    The phrase to get the acronym from.
+    
+    .EXAMPLE
+    Get-Acronym -Phrase "As Soon As Possible"
+    #>
+    [CmdletBinding()]
+    Param (
+        [string]$Phrase
+    )
+    Throw "Please implement this function"
+}
