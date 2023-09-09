421db195f2560db3e6227b208ea63d8f3245fafb
glaxxie <86179463+glaxxie@users.noreply.github.com>
2023-09-09 02:31:14 -0500

    add resistor color trio (#265)

---
+++ exercises/practice/resistor-color-trio/ResistorColorTrio.ps1
@@ -0,0 +1,21 @@
+Function Get-ResistorLabel() {
+    <#
+    .SYNOPSIS
+    Implement a function to get the label of a resistor with three color-coded bands.
+
+    .DESCRIPTION
+    Given an array of colors from a resistor, decode their resistance values and return a string represent the resistor's label.
+
+    .PARAMETER Colors
+    The array repesent the 3 colors from left to right.
+
+    .EXAMPLE
+    Get-ResistorLabel -Colors @("red", "white", "blue")
+    Return: "29 megaohms"
+     #>
+    [CmdletBinding()]
+    Param(
+        [string[]]$Colors
+    )
+    Throw "Please implement this function"
+}
