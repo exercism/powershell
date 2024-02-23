c06960f3df5958384b9cef905f9591ea0d0cdbd4
glaxxie <86179463+glaxxie@users.noreply.github.com>
2024-02-23 14:42:42 -0600

    Update test suites
    Simplify test files for:
    - Armstrong Numbers
    - Isbn Verifier
    -Isogram
    -Matching Brackets
    -Pangram

--- exercises/practice/isbn-verifier/IsbnVerifier.tests.ps1
+++ exercises/practice/isbn-verifier/IsbnVerifier.tests.ps1
@@ -6,114 +6,114 @@ Describe "IsbnVerifierTests" {
 	It "valid isbn" {
         $got = Test-Isbn -Isbn "3-598-21508-8"
 
-		$got | Should -Be $true
+		$got | Should -BeTrue
 	}
 
     It "invalid isbn check digit" {
         $got = Test-Isbn -Isbn "3-598-21508-9"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "valid isbn with a check digit of 10" {
         $got = Test-Isbn -Isbn "3-598-21507-X"
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
 
     It "check digit is a character other than X" {
         $got = Test-Isbn -Isbn "3-598-21507-A"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "invalid check digit in isbn is not treated as zero" {
         $got = Test-Isbn -Isbn "4-598-21507-B"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "invalid character in isbn is not treated as zero" {
         $got = Test-Isbn -Isbn "3-598-P1581-X"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "X is only valid as a check digit" {
         $got = Test-Isbn -Isbn "3-598-2X507-9"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "valid isbn without separating dashes" {
         $got = Test-Isbn -Isbn "3598215088"
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
 
     It "isbn without separating dashes and X as check digit" {
         $got = Test-Isbn -Isbn "359821507X"
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
 
     It "isbn without check digit and dashes" {
         $got = Test-Isbn -Isbn "359821507"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "too long isbn and no dashes" {
         $got = Test-Isbn -Isbn "3598215078X"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "too short isbn" {
         $got = Test-Isbn -Isbn "00"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "isbn without check digit" {
         $got = Test-Isbn -Isbn "3-598-21507"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "check digit of X should not be used for 0" {
         $got = Test-Isbn -Isbn "3-598-21515-X"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "empty isbn" {
         $got = Test-Isbn -Isbn ""
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "input is 9 characters" {
         $got = Test-Isbn -Isbn "134456729"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "invalid characters are not ignored after checking length" {
         $got = Test-Isbn -Isbn "3132P34035"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "invalid characters are not ignored before checking length" {
         $got = Test-Isbn -Isbn "3598P215088"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 
     It "input is too long but contains a valid isbn" {
         $got = Test-Isbn -Isbn "98245726788"
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
 }
--- exercises/practice/matching-brackets/MatchingBrackets.tests.ps1
+++ exercises/practice/matching-brackets/MatchingBrackets.tests.ps1
@@ -6,121 +6,121 @@ Describe "Matching brackets test cases" {
     It "paired square brackets" {
         $got  = Test-MatchingBrackets("[]")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "empty string" {
         $got  = Test-MatchingBrackets("")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "unpaired brackets" {
         $got  = Test-MatchingBrackets("[[")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "wrong ordered brackets" {
         $got  = Test-MatchingBrackets("}{")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "wrong closing bracket" {
         $got  = Test-MatchingBrackets("{]")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "paired with whitespace" {
         $got  = Test-MatchingBrackets("{  }")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "partially paired brackets" {
         $got  = Test-MatchingBrackets("{[])")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "simple nested brackets" {
         $got  = Test-MatchingBrackets("{[]}")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "several paired brackets" {
         $got  = Test-MatchingBrackets("{}[]")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "paired and nested brackets" {
         $got  = Test-MatchingBrackets("([{}({}[])])")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "unopened closing brackets" {
         $got  = Test-MatchingBrackets("{[)][]}")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "unpaired and nested brackets" {
         $got  = Test-MatchingBrackets("([{])")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "paired and wrong nested brackets" {
         $got  = Test-MatchingBrackets("[({]})")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "paired and wrong nested brackets but innermost are correct" {
         $got  = Test-MatchingBrackets("[({}])")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "paired and incomplete brackets" {
         $got  = Test-MatchingBrackets("{}[")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "too many closing brackets" {
         $got  = Test-MatchingBrackets("[]]")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "early unexpected brackets" {
         $got  = Test-MatchingBrackets(")()")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "early mismatched brackets" {
         $got  = Test-MatchingBrackets("{)()")
 
-        $got | Should -Be $false
+        $got | Should -BeFalse
     }
     
     It "math expression" {
         $got  = Test-MatchingBrackets("(((185 + 223.85) * 15) - 543)/2")
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
     
     It "complex latex expression" {
         $complexString = "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)"
         $got  = Test-MatchingBrackets($complexString)
 
-        $got | Should -Be $true
+        $got | Should -BeTrue
     }
 }
