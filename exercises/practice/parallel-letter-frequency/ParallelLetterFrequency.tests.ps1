3fcf691665e1ef06c84996ee99981eac4624e59d
rzuckerm <rzdulc@gmail.com>
2025-11-11 09:53:31 -0600

    Fix comment in Parallel Letter Frequency Tests

--- exercises/practice/parallel-letter-frequency/ParallelLetterFrequency.tests.ps1
+++ exercises/practice/parallel-letter-frequency/ParallelLetterFrequency.tests.ps1
@@ -117,7 +117,7 @@ Describe "ParallelLetterFrequency test cases" {
         Test-HashtableEquality $got $want
     }
 
-    powershell track exclusive test
+    # powershell track exclusive test
     It "massive lorem ipsum texts" {
         $texts = @(
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." * 237
